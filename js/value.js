d0249d4bd45c8c4aae76e737b72d437fe57d661b
Karina2023h <127620416+Karina2023h@users.noreply.github.com>
2024-03-24 12:50:12 +0100

    bvjhv

--- js/value.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("beforeBtn").addEventListener("click", function () {
-  document.getElementById("priceRange").value = 0;
-});
-
-document.getElementById("afterBtn").addEventListener("click", function () {
-  document.getElementById("priceRange").value = 100;
-});
